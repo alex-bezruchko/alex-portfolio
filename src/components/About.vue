732b0780ff8d209e694cf7f6c407fd2f6794a39b
AlexBezruchko <alex.bezruchko@gmail.com>
2023-02-26 13:40:27 -0500

    added About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>About me</div>
+    <div class="container">
+        <h1 class="page-header">About Me...</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive imgshadow" id="mountain-jpg" src="./../img/mountain.jpg" alt="mountain-pic"
+                    style="width: 100%">
+            </div>
+            <div class="col-md-8">
+                <div class="col-lg-12">
+                    <p>A little over two years ago, I wrote my first line of code and was
+                        hooked into the challenging but cool world of programming. At the time, I was a health insurance
+                        agent which while was financially rewarding career was not the most exciting of challenges. I worked
+                        through some courses on Codecademy, Team TreeHouse, Udemy among other and began to imagine coding as
+                        much fun as well as a rewarding experience.
+                    </p>
+                </div>
+
+                <div class="col-lg-12 pt-4 pt-lg-0">
+                    <p class="second px-8">After picking up the basics, I've learned CMS php frameworks such as Drupal and
+                        WordPress as well as NodeJS (backend), ReactJS and VueJs (frontend) for my projects with great
+                        results. The learning has continued and coding has turned from a hobby into a passion that I want to
+                        pursue each and every day. I’m a self-taught developer with a passion for code and what it can add
+                        to our work, our communities, and our world.
+                    </p>
+                </div>
+                <div class="col-lg-12">
+                    <p class="third">I'm curently working as a contractor but I'm always looking for permanent
+                        opportunities. About myself, I love hiking, grilling mean food, and watching <strong>The
+                            Arsenal</strong>.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
