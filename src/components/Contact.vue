d29cbd1187f493ccb7237115a4a02f68a6922d13
AlexBezruchko <alex.bezruchko@gmail.com>
2023-02-26 13:41:00 -0500

    added Contact section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>Contact</div>
+    <div class="container">
+        <h1 class="page-header">Contact</h1>
+        <div class="container flex column">
+            <a href="https://github.com/alex-bezruchko"><span><i class="bi bi-github text-center orange"
+                        aria-hidden="true"></i> Github</span>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
