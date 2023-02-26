78112a28284adde710ead5511773c9fd50e7b9a8
AlexBezruchko <alex.bezruchko@gmail.com>
2023-02-26 13:41:36 -0500

    added Portfolio section

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,123 @@
 <template>
-    <div>Portfolio</div>
+    <div class="container portfolio">
+        <h1 class="page-header">Portfolio</h1>
+
+        <div class="row">
+            <div class="col col-sm-12 col-lg-4">
+                <div id="car1" class="carousel slide" data-bs-ride="car1">
+                    <div class="carousel-indicators">
+                        <button class="indicators active" type="button" data-bs-target="#car1" data-bs-slide-to="0"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button class="indicators" type="button" data-bs-target="#car1" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button class="indicators" type="button" data-bs-target="#car1" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./../img/portfolio/lilit/lilit1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/lilit/lilit2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/lilit/lilit3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#car1" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#car1" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod lorem
+                    non
+                    erat tincidunt, id varius arcu malesuada.</p>
+            </div>
+            <div class="col col-sm-12 col-lg-4">
+                <div id="car2" class="carousel slide" data-bs-ride="car2">
+                    <div class="carousel-indicators">
+                        <button class="indicators active" type="button" data-bs-target="#car2" data-bs-slide-to="0"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button class="indicators" type="button" data-bs-target="#car2" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button class="indicators" type="button" data-bs-target="#car2" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./../img/portfolio/raffi/raffi1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/raffi/raffi2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/raffi/raffi3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#car2" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#car2" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod lorem
+                    non
+                    erat tincidunt, id varius arcu malesuada.</p>
+            </div>
+            <div class="col col-sm-12 col-lg-4">
+                <div id="car3" class="carousel slide" data-bs-ride="car3">
+                    <div class="carousel-indicators">
+                        <button class="indicators active" type="button" data-bs-target="#car3" data-bs-slide-to="0"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button class="indicators" type="button" data-bs-target="#car3" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button class="indicators" type="button" data-bs-target="#car3" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                        <button class="indicators" type="button" data-bs-target="#car3" data-bs-slide-to="3"
+                            aria-label="Slide 3"></button>
+                        <button class="indicators" type="button" data-bs-target="#car3" data-bs-slide-to="4"
+                            aria-label="Slide 4"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./../img/portfolio/wineblog/wineblog1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/wineblog/wineblog2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/wineblog/wineblog3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/wineblog/wineblog4.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./../img/portfolio/wineblog/wineblog5.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#car3" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#car3" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod lorem
+                    non
+                    erat tincidunt, id varius arcu malesuada.</p>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
