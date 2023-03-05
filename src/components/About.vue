2b086c2f84dc96f9a11d0c0a6af6d18ce52b022b
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-04 19:50:17 -0500

    updated about text

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,24 +8,25 @@
             </div>
             <div class="col-md-8">
                 <div class="col-lg-12">
-                    <p>A little over five years ago, I wrote my first line of code and was
-                        hooked into the challenging but cool world of programming. At the time, I was a health insurance
-                        agent which while was financially rewarding career was not the most exciting of challenges. I worked
-                        through some courses on Codecademy, Team TreeHouse, Udemy among other and began to imagine coding as
-                        much fun as well as a rewarding experience.
+                    <p>
+                        About five years ago, I wrote my first line of code and was hooked. At the time I was a health
+                        insurance agent, which while a financially rewarding career, was not my passion. I decided to
+                        continue exploring programming. I worked through some courses (Codecademy, TreeHouse and Udemy,
+                        etc), and began to imagine coding as a career; the more I learned, the more I realized that it was
+                        not only intellectually rewarding, but I loved it!
                     </p>
                 </div>
                 <div class="col-lg-12 pt-4 pt-lg-0">
-                    <p class="px-8">After picking up the basics, I've learned CMS php frameworks such as Drupal and
-                        WordPress as well as NodeJS (backend), ReactJS and VueJs (frontend) for my projects with great
-                        results. The learning has continued and coding has turned from a hobby into a passion that I want to
-                        pursue each and every day. I’m a self-taught developer with a passion for code and what it can add
-                        to our work, our communities, and our world.
+                    <p class="px-8">
+                        After picking up the basics, I tried CMS php frameworks, such as Drupal and WordPress, but quickly
+                        moved on to Laravel, NodeJS (backend) and ReactJS and VueJs (frontend) for my projects; all with
+                        great results. I’m a self-taught developer, and my learning has continued, and coding has turned
+                        from a hobby into a passion.
                     </p>
                 </div>
                 <div class="col-lg-12">
-                    <p>I'm curently working as a contractor but I'm always looking for permanent opportunities. About
-                        myself, I love hiking, grilling mean food, and watching <strong>The Arsenal</strong>.
+                    <p>I'm currently working as a contractor but am looking for permanent opportunities, and new challenges.
+                        About myself, I love hiking, grilling mean food, and watching <strong>The Arsenal</strong>.
                     </p>
                 </div>
             </div>
